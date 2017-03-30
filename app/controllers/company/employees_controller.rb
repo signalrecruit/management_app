3d273675cbff2ccad0224deb02f494e994497643
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-03-30 23:44:25 +0000

    commit changes

--- app/controllers/company/employees_controller.rb
+++ app/controllers/company/employees_controller.rb
@@ -1,6 +1,6 @@
 class Company::EmployeesController < Company::ApplicationController
-   before_action :set_applicant_detail, except: [:index, :complete, :send_employee_to_company]
-  before_action :set_employee, except: [:index, :complete, :send_employee_to_company]
+  before_action :set_applicant_detail, except: [:index]
+  before_action :set_employee, except: [:index]
   layout 'company'
 
   def index
@@ -9,42 +9,10 @@ class Company::EmployeesController < Company::ApplicationController
 
   def show
   end
+
   
-  def new
-    @employee = @applicant_detail.build_employee
-  end
 
-  def create
-    new
-    create_employee_from_applicant_detail(@applicant_detail, @employee)
-  end
-
-  def edit
-  end
-
-  def update
-    if @employee.update(employee_params)
-      on_success "successfully created employee profile", [:recruiter, @applicant_detail, @employee]
-    else
-      on_failure "oops! something went wrong", [:edit, :recruiter, @applicant_detail, @employee]
-    end
-  end
-
-  def destroy
-  end
-
-  def complete
-    @employee = Employee.find(params[:id])
-    @employee.update(profile_status: "COMPLETE") if profile_is_completed?(@employee)
-    redirect_to recruiter_employees_url
-  end
-
-  def send_employee_to_company
-    @employee = Employee.find(params[:id])
-    @employee.update(sent: true) if @employee.profile_status == "COMPLETE"
-    redirect_to recruiter_employees_url
-  end
-
+ 
 
 
   private
@@ -78,20 +46,5 @@ class Company::EmployeesController < Company::ApplicationController
   def on_failure(msg, path)
     flash[:error] = msg
     render path
-  end
-
-  def create_employee_from_applicant_detail(applicant_detail, employee)
-    employee.surname = applicant_detail.lastname
-    employee.firstname = applicant_detail.firstname
-    employee.email_address = applicant_detail.email
-    employee.mobile_number = applicant_detail.phonenumber
-    employee.position = applicant_detail.job_title
-    employee.company_id = applicant_detail.requirement.company_id
-
-    if employee.save
-      on_success "please complete employee profile", [:edit, :recruiter, applicant_detail, employee]
-    else
-      on_failure "oops something went wrong", :back
-    end
   end
 end
