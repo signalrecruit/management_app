7787916ae5891814b7d88660c625769cbcc09cc7
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:17:29 +0000

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,15 +5,15 @@ class Event < ActiveRecord::Base
   validates :name, :interview_date, :test_date, presence: true
 
   def passed_interview?
-  	return true if self.interview_results == "PASSED"
+  	self.interview_results == "PASSED"
   end
 
   def failed_interview?
-  	return true if self.interview_results == "FAILED"
+  	self.interview_results == "FAILED"
   end
 
   def pending_interview?
-    return true if self.interview_results  == "PENDING"
+    self.interview_results  == "PENDING"
   end
 
   def sent_by_recruiter?
