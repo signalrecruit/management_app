06b6da1dc3c6885e7fc9f81562500916f5b6d0d2
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:15:24 +0000

    Update employment_history.rb

--- app/models/employment_history.rb
+++ app/models/employment_history.rb
@@ -2,6 +2,6 @@ class EmploymentHistory < ActiveRecord::Base
   belongs_to :employee
 
   def update_button_clicked?
-  	return true if self.update_button == true
+  	self.update_button == true
   end
 end
